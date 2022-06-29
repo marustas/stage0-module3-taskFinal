558e2e4eea0713d1aace6b190a99cb99baf93240
Stanislau Marudau <marudov.stas@gmail.com>
2022-06-29 14:40:19 +0300

    Third try of final task

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,7 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
-       double temperatureFahrenheit=temperatureCelsius+32.0;
+       double temperatureFahrenheit=temperatureCelsius*1.8+32.0;
         System.out.println(temperatureFahrenheit);
     }
 }
