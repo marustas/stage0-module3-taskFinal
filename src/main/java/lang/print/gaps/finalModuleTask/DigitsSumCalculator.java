cbfc146942a89ddc043d21423889581ac7d7ee6f
Stanislau Marudau <marudov.stas@gmail.com>
2022-06-29 14:04:10 +0300

    First try of final task

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        while (number > 0) {
+            int digit;
+            int sum = 0;
+            digit = number % 10;
+            sum = sum + digit;
+            number = number / 10;
+            System.out.println(sum);
+        }
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+
+        int reversed = 0;
+
+        for (; number != 0; number /= 10) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+        }
+
+        System.out.println("Reversed Number: " + reversed);
     }
 }
